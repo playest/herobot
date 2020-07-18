3a7a1d417a46f04c8a7ac759c05dab96ed751e79
Anonymous <anon@ymo.us>
2020-07-18 17:31:29 +0200

    working except for watching commands

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::path::{Path, PathBuf};
-use std::sync::{mpsc::channel};
+use std::sync::{mpsc::{Receiver, channel}};
 use std::thread::{self};
 use std::{time::Duration};
 use telegram_bot::*;
 use tokio::{prelude::*, stream::{StreamExt}};
 use thread::sleep;
+use futures::{pin_mut, select, join};
 
 struct Bot {
     pub api: Api,
@@ -18,25 +19,39 @@ struct Bot {
     pub status_indicator: MessageOrChannelPost,
     pub status_indicator_oldness: i8,
     pub in_error: bool,
+    watch_dir: PathBuf,
+    stream: UpdatesStream,
+    rx: Receiver<DebouncedEvent>,
+    watcher: RecommendedWatcher,
 }
 
 impl Bot {
-    fn new(api: Api, recipient: UserId, message: MessageOrChannelPost) -> Bot {
+    fn new(api: Api, recipient: UserId, message: MessageOrChannelPost, watch_dir: PathBuf) -> Bot {
+        let api = api;
+        let stream = api.stream();
+        let (tx, rx) = channel();
+        let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2)).unwrap();
+        watcher.watch(&watch_dir, RecursiveMode::Recursive).unwrap();
         Bot {
             api,
             recipient,
             status_indicator: message,
             status_indicator_oldness: 0,
             in_error: false,
+            watch_dir,
+            stream,
+            rx,
+            watcher,
         }
     }
 
-    fn error(&mut self, message: &str, err: Error) {
-        self.in_error = true;
+    fn error(&self, message: &str, err: Error) {
+        //self.in_error = true;
         eprintln!("{} {}", message, err)
     }
 
-    async fn update_status_indicator(&mut self) {
+    async fn update_status_indicator(&self) {
+        println!("Update status");
         if self.status_indicator_oldness < 5 {
             match self
                 .api
@@ -46,16 +61,16 @@ impl Bot {
                 )))
                 .await
             {
-                Ok(_) => self.status_indicator_oldness = 0,
+                Ok(_) => { /*self.status_indicator_oldness = 0*/ },
                 Err(err) => self.error("Cannot update status indicator", err),
             };
         }
     }
 
-    async fn send(&mut self, text: &str) {
+    async fn send(&self, text: &str) {
         match self.api.send(self.recipient.text(text)).await {
             Ok(_) => {
-                self.status_indicator_oldness += 1;
+                //self.status_indicator_oldness += 1;
             }
             Err(err) => {
                 self.error("Cannot send message because", err);
@@ -63,7 +78,7 @@ impl Bot {
         };
     }
 
-    async fn maybe_notify(&mut self, path: &PathBuf) {
+    async fn maybe_notify(&self, path: &PathBuf) {
         let file = File::open(path.to_owned()).unwrap();
         let mut reader = BufReader::new(file);
         let mut buffer = String::new();
@@ -79,9 +94,41 @@ impl Bot {
             Err(err) => eprintln!("Error: {}", err),
         }
     }
+
+    async fn watch_files(&self) {
+        println!("Watch files");
+        for _ in 1..10 {
+            let r = self.rx.try_recv();
+            match r {
+                Ok(DebouncedEvent::Write(path)) => {
+                    self.maybe_notify(&path).await;
+                },
+                _ => { }
+            }
+        }
+    }
+
+    async fn watch_commands(&mut self) {
+        println!("Watch commands");
+        let update = self.stream.try_next().await;
+        println!("update: {:?}", update);
+        match update {
+            //Some(Ok(Ok(update))) => { // next
+            Ok(Some(update)) => { // try_next
+                if let UpdateKind::Message(message) = update.kind {
+                    if let MessageKind::Text { ref data, .. } = message.kind {
+                        if data == "/status" {
+                            show_status(self).await;
+                        }
+                    }
+                }
+            },
+            _ => { }
+        }
+    }
 }
 
-async fn show_status(bot: &mut Bot) {
+async fn show_status(bot: &Bot) {
     bot.send("Status").await;
 }
 
@@ -89,9 +136,6 @@ async fn watch(path: &PathBuf, bot: &mut Bot) -> () {
     let (tx, rx) = channel();
     let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2)).unwrap();
     watcher.watch(path, RecursiveMode::Recursive).unwrap();
-
-    // This is a simple loop, but you may want to use more complex logic here,
-    // for example to handle I/O.
 
     loop {
         let r = rx.recv();
@@ -143,59 +187,27 @@ async fn main() -> Result<(), Error> {
     //println!("id: {}", id);
     //println!("First message");
     let first_message = api.send(id.text("Herobot is back!").disable_notification()).await?;
-    let mut bot = Bot::new(api, id, first_message);
+    let mut bot = Bot::new(api, id, first_message, watch_dir_path);
     //let bot = Arc::new(Mutex::new(bot));
     //let mut bot = Arc::new(RefCell::new(Bot::new(api, id, first_message)));
     //file_watcher.await;
     //let file_watcher = watch(&watch_dir_path, &mut bot);
     //file_watcher.await;
 
-    let (tx, rx) = channel();
-    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2)).unwrap();
-    watcher.watch(&watch_dir_path, RecursiveMode::Recursive).unwrap();
 
-    //let mut stream = bot.api.stream();
-    //let mut stream = stream.timeout(Duration::from_secs(1));
-    //stream.
-    //let mut stream2 = stream.timeout(Duration::from_millis(1000));
-    //let mut stream2 = stream;
     let mut stream2 = bot.api.stream().timeout(Duration::from_secs(5));
 
+    //let watch_files = bot.watch_files();
+    //let watch_commands = bot.watch_commands();
+    //let update_status = bot.update_status_indicator();
+
     loop {
-        println!("Watch files");
-        for _ in 1..10 {
-            let r = rx.try_recv();
-            match r {
-                Ok(DebouncedEvent::Write(path)) => {
-                    bot.maybe_notify(&path).await;
-                },
-                _ => { }
-            }
-        }
-
-        println!("Watch commands");
+        let watch_files = bot.watch_files();
+        //let watch_commands = bot.watch_commands();
+        let update_status = bot.update_status_indicator();
         
-        /**/
-        let update = stream2.try_next().await;
-        //let update = stream2.timeout(Duration::from_secs(2)).next().await;
-        println!("update: {:?}", update);
-        match update {
-            //Some(Ok(Ok(update))) => { // next
-            Ok(Some(Ok(update))) => { // try_next
-                if let UpdateKind::Message(message) = update.kind {
-                    if let MessageKind::Text { ref data, .. } = message.kind {
-                        if data == "/status" {
-                            show_status(&mut bot).await;
-                        }
-                    }
-                }
-            },
-            _ => { }
-        }
-        /**/
-
-        println!("Update status indicator");
-        bot.update_status_indicator().await;
+        //pin_mut!(watch_files, update_status);
+        join!(watch_files, update_status);
 
         println!("Sleep");
         sleep(Duration::from_secs(2));
