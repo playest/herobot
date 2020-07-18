e1a34a29242cf8bfeae164de743a7101b983e122
Anonymous <anon@ymo.us>
2020-07-18 14:11:53 +0200

    working but not in multi thread

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::env;
 
-use std::fs::File;
-use futures::StreamExt;
+use chrono::prelude::*;
 use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 use std::path::{Path, PathBuf};
-use std::sync::mpsc::channel;
-use std::thread::sleep;
+use std::sync::{mpsc::channel, Arc};
+use std::thread::{self};
 use std::{time::Duration};
 use telegram_bot::*;
-use std::io::{self, prelude::*, BufReader};
+use tokio::{sync::Mutex, prelude::*};
 
 struct Bot {
     pub api: Api,
@@ -38,10 +39,10 @@ impl Bot {
         if self.status_indicator_oldness < 5 {
             match self
                 .api
-                .send(
-                    self.status_indicator
-                        .edit_text(format!("Bot last active pinged at {}", "now")),
-                )
+                .send(self.status_indicator.edit_text(format!(
+                    "Bot last active pinged at {}",
+                    Local::now().trunc_subsecs(0)
+                )))
                 .await
             {
                 Ok(_) => self.status_indicator_oldness = 0,
@@ -62,45 +63,50 @@ impl Bot {
     }
 }
 
-fn watch(path: PathBuf) -> notify::Result<()> {
+async fn watch(path: &PathBuf, bot: &mut Bot) -> () {
     // Create a channel to receive the events.
     let (tx, rx) = channel();
 
     // Automatically select the best implementation for your platform.
     // You can also access each implementation directly e.g. INotifyWatcher.
-    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2))?;
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2)).unwrap();
 
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
-    watcher.watch(path, RecursiveMode::Recursive)?;
+    watcher.watch(path, RecursiveMode::Recursive).unwrap();
 
     // This is a simple loop, but you may want to use more complex logic here,
     // for example to handle I/O.
+
     loop {
-        match rx.recv() {
+        let r = rx.recv();
+        match r {
             Ok(event) => {
-                if let DebouncedEvent::Write(path) = event {
-                    println!("write in {:?}", path);
-                    let file = File::open(path.to_owned()).unwrap();
-                    let mut reader = BufReader::new(file);
-                    let mut buffer = String::new();
-                    match reader.read_line(&mut buffer) {
-                        Ok(_) => {
-                            println!("first line of {}: {}", path.to_string_lossy(), buffer);
-                            if buffer.trim() != "ok" {
-                                println!("News in {}", path.to_string_lossy());
+                match event {
+                    DebouncedEvent::Write(path) => {
+                        println!("write in {:?}", path);
+                        let file = File::open(path.to_owned()).unwrap();
+                        let mut reader = BufReader::new(file);
+                        let mut buffer = String::new();
+                        match reader.read_line(&mut buffer) {
+                            Ok(_) => {
+                                //println!("first line of {}: {}", path.to_string_lossy(), buffer);
+                                if buffer.trim() != "ok" {
+                                    let msg =
+                                        format!("{} not ok: {}", path.to_string_lossy(), buffer);
+                                    bot.send(&msg).await;
+                                    bot.update_status_indicator().await;
+                                    println!("News in {}", path.to_string_lossy());
+                                }
                             }
-                            else {
-                                println!("No news in {}", path.to_string_lossy());
-                            }
-                        },
-                        Err(err) => eprintln!("Error: {}", err),
+                            Err(err) => eprintln!("Error: {}", err),
+                        }
                     }
-                    println!("end!");
+                    _ => {}
                 }
-            },
+            }
             Err(e) => eprintln!("watch error: {:?}", e),
-        }
+        };
     }
 }
 
@@ -126,12 +132,16 @@ async fn main() -> Result<(), Error> {
     let api = Api::new(token);
 
     let id: UserId = UserId::from(recipient_id);
-    println!("id: {}", id);
-    println!("First message");
-    api.send(id.text("message to you").disable_notification())
-        .await?;
+    //println!("id: {}", id);
+    //println!("First message");
+    let first_message = api.send(id.text("Herobot is back!").disable_notification()).await?;
+    let mut bot = Bot::new(api, id, first_message);
+    //let bot = Arc::new(Mutex::new(bot));
+    //let mut bot = Arc::new(RefCell::new(Bot::new(api, id, first_message)));
 
-    watch(watch_dir_path);
+    //file_watcher.await;
+    let file_watcher = watch(&watch_dir_path, &mut bot);
+    file_watcher.await;
 
     Ok(())
 }
